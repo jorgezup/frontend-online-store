a92a6fe2603a39d0ac19d5610a93198ddc933689
Maiara Borges <maiaraborgesamd@gmail.com>
2022-02-09 16:52:39 -0300

    Fix error

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,7 @@
 export async function getCategories() {
-  const result = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  const data = await result.json();
-  console.log(data);
-  return data;
+  // Implemente aqui
 }
 
-export async function getProductsFromCategoryAndQuery(category) {
-  const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${category}`);
-  const data = await result.json();
-  console.log(data);
-  return data;
+export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
 }
