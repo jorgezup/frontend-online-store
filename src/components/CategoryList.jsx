4d45ee60995f6d587310af3d152d7dd62235df0b
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-02-09 17:50:28 -0400

    Requirement 4

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { getCategories } from '../services/api';
+
+class CategoryList extends Component {
+  state = {
+    categories: [],
+  }
+
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  fetchApi = async () => {
+    const categories = await getCategories();
+    this.setState({
+      categories,
+    });
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div>
+        <ul>
+          {categories
+          && categories.map(({ id, name }) => (
+            <li key={ id }>
+              <button
+                data-testid="category"
+                type="button"
+              >
+                {name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default CategoryList;
