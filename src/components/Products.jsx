0493b3d5ec6f4d8d9670847b39f2e6b7d17923e4
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-02-10 17:25:16 -0400

    Requirement 5: fix Lint problems

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Card from './Card';
 
 class Products extends React.Component {
-    render() {
-        const { list } = this.props;
-        console.log(list)
-        return (
-            <div>
-                {list.map(({price, title, thumbnail, id}) => ( 
-                    <Card title={title}  price={price} url={thumbnail} key={id} />
-                ))}
-            </div>
-        )
-    }
+  render() {
+    const { list } = this.props;
+    return (
+      <div>
+        {list.map(({ price, title, thumbnail, id }) => (
+          <Card title={ title } price={ price } url={ thumbnail } key={ id } />
+        ))}
+      </div>
+    );
+  }
 }
 
-
+Products.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Products;
