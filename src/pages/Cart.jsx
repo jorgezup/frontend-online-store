8768e88c931213ca574c68b5df5e268e9cf6bd6e
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-02-13 21:03:41 -0400

    icons add, remove, delete

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import remove from '../icons/minus.png';
+import add from '../icons/plus.png';
+import del from '../icons/delete.png';
 
 class Cart extends Component {
   render() {
-    const { productList } = this.props;
+    const { productList,
+      handleRemoveProduct,
+      handleAddProduct,
+    } = this.props;
     return (
       <div>
         {
-          productList.length ? productList.map(({ price, title, thumbnail, id }) => (
-            <div key={ id }>
-              <h2 data-testid="shopping-cart-product-name">{ title }</h2>
-              <img src={ thumbnail } alt={ title } />
-              <p>{ price }</p>
-              <p data-testid="shopping-cart-product-quantity">1</p>
+          productList.length ? productList.map((product) => (
+            <div key={ product.id }>
+              <h2 data-testid="shopping-cart-product-name">{ product.title }</h2>
+              <img src={ product.thumbnail } alt={ product.title } />
+              <p>{ product.price }</p>
+              <span data-testid="shopping-cart-product-quantity">{product.amount}</span>
+              <button
+                type="button"
+                value={ product.id }
+                onClick={ () => handleRemoveProduct(product) }
+                data-testid="product-decrease-quantity"
+              >
+                <img src={ remove } alt="Remover um item" />
+              </button>
+              <button
+                type="button"
+                value={ product.id }
+                onClick={ () => handleAddProduct(product) }
+                data-testid="product-increase-quantity"
+              >
+                <img src={ add } alt="Adicionar mais um item" />
+              </button>
+              <button type="button">
+                <img src={ del } alt="Remover produto do carrinho" />
+              </button>
             </div>
           )) : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
         }
@@ -23,6 +48,8 @@ class Cart extends Component {
 
 Cart.propTypes = {
   productList: PropTypes.arrayOf(PropTypes.object),
+  handleAddProduct: PropTypes.func.isRequired,
+  handleRemoveProduct: PropTypes.func.isRequired,
 };
 
 Cart.defaultProps = {
